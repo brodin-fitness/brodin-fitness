49a31f776d76826138594ab6ec2dfa14341bcefa
zjcrumpton <Zaccary.Crumpton@charter.com>
2022-06-03 21:12:03 -0600

    create routing boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App" />
+    <Routes>
+      <Route path="/" element={null} />
+      <Route path="/fitness" element={null} />
+      <Route path="/focus" element={null} />
+      <Route path="/food" element={null} />
+      <Route path="/account" element={null} />
+    </Routes>
   );
 }
 
